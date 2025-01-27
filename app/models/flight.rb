f567e1bde1594a0872d97ec2dd0a9545efaab45d
Brian Sitati <briansit93@gmail.com>
2025-01-27 14:59:20 +0300

    set up requisite code for the models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport
-  belongs_to :arrival_airport
+  belongs_to :departure_airport, class_name: "Airport"
+  belongs_to :arrival_airport, class_name: "Airport"
+
+  def self.available_dates
+    pluck("DATE(departure_time)").uniq
+  end
 end
